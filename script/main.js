812f19b4488bb5617507856c5f8848616d5f5962
landrykf <landrykf@gmail.com>
2020-11-16 05:50:21 +0100

    ajout de vue js au projet

--- script/main.js
+++ script/main.js
@@ -3,7 +3,14 @@ const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 const reseauxAnime = document.querySelectorAll('.reseau');
 const flecheAnime = document.querySelector('.arrowRight');
+const flecheAnimeLeft =document.querySelector('.arrowLeft')
 const textApparition= document.querySelector('.textApparition');
+const textApparitionLeft = document.querySelector('.textApparitionLeft')
+const project1 = document.querySelectorAll('.project1');
+const project2 = document.querySelectorAll('.project2')
+let filterElementTab = [];
+
+// console.log(project2)
 
 // const showContent = () => {
 //     document.querySelector('.loaderContainer').classList.add('hidden');
@@ -16,81 +23,61 @@ burger.addEventListener('click', () => {
     overlay.classList.toggle('overlayOpen');
 });
 
+if (flecheAnime != null) {
+    flecheAnime.addEventListener('mouseover',() =>{
+        flecheAnime.classList.add('ouverture');
+        textApparition.classList.add('apparition')
+    });
+    
+    flecheAnime.addEventListener('mouseleave',() =>{
+        textApparition.classList.remove('apparition')
+    });
+}
 
-flecheAnime.addEventListener('mouseover',() =>{
-    flecheAnime.classList.add('ouverture');
-    textApparition.classList.add('apparition')
-});
-
-flecheAnime.addEventListener('mouseleave',() =>{
-    textApparition.classList.remove('apparition')
-});
-
-// function ProjectFilter(personnage) {
-//     // Declare variables
-//     let projectFil = document.getElementById("personnagesSearch");
-//     let persoDiv = document.querySelectorAll('.persoDiv')
-
-
-//     personnagesSearch.addEventListener('keyup', () => {
-//         let input = personnagesSearch.value;
-//         let result = personnage.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
-//         let suggestion = "";
-//         if (input != '' && input != " ") {
-//             result.forEach(resultItem => suggestion += `<div class = "suggestion"><p>${resultItem.name}</p><div>`)
-//         }
-//         document.getElementById('suggestions').innerHTML = suggestion;
+if (flecheAnimeLeft != null) {
+    flecheAnimeLeft.addEventListener('mouseover',() =>{
+        flecheAnimeLeft.classList.add('ouverture');
+        textApparitionLeft.classList.add('apparition')
+    });
+    
+    flecheAnimeLeft.addEventListener('mouseleave',() =>{
+        textApparitionLeft.classList.remove('apparition')
+    });
+}
 
 
-//         //trier les elements dans ma barre de recherche et les afficher
-//         for (let i = 0; i < persoDiv.length; i++) {
-//             let inclue = personnage[i].name.toLocaleLowerCase().includes(input.toLocaleLowerCase());
-//             // console.log(suggestion)
+projectFilter('all')
+
+function projectFilter(value) {
+    let box = document.getElementsByClassName('box')
+    if (value == 'all') value ='';
+    for (let i = 0; i < box.length; i++) {
+        removeClass(box[i],"show");
+        if(box[i].className.indexOf(value) > -1){
+            addClass(box[i],"show")
+        }
+    }
+}
+
+function addClass(element, name) {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+    for (i=0; i < arr2.length; i++){
+        if(arr1.indexOf(arr2[i] == -1)){
+          element.className +=" " + arr2[i];   
+        }
+    }
+}
+
+function removeClass(element, name) {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++){
+        while(arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1)
+        }
+    }
+    element.className = arr1.join(" ")
+}
 
 
-
-//             if (!inclue) {
-//                 persoDiv[i].classList.add('inclue');
-//             } else {
-//                 persoDiv[i].classList.remove('inclue')
-//             };
-
-//         }
-//     })
-// }
-// ProjectFiter("all")
-
-// function ProjectFiter(c) {
-//     let box,i;
-
-//     box = document.querySelectorAll(".box");
-//     if(c == "all") c="";
-//     for (i =0;i <box.length;i++){
-//         removeClass(box[i],"show");
-//         if(box[i].className.indexOf(c)>-1) addClass(box[i],"show")
-//     }
-//     console.log(box[i])
-// }
-
-// function addClass(element, name) {
-//     let i, arr1,arr2;
-//     arr1 = element.className.split("");
-//     arr2 = name.split("");
-//     for(i=0;i<arr2.length;i++){
-//         if(arr1.indexOf(arr2[i])==-1){
-//             element.className += "" + arr2[i];
-//         }
-//     }
-// }
-
-// function removeClass(element,name) {
-//     let i,arr1,arr2;
-//     arr1 = element.className.split("");
-//     arr2 = name.split("");
-//     for (i = 0; i < arr2.length;i++){
-//         while (arr1.indexOf(arr2[i]) > -1){
-//             arr1.splice(arr1.indexOf(arr2[i]), 1);
-//         }
-//     }
-//     element.className = arr1.join(" ");
-// }
